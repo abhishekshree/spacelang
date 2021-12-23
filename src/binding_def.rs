9379d13cd677c6733cecc15da18686358eb745e1
Abhishek Shree <abhi1234456@gmail.com>
2021-12-23 22:24:21 +0530

    Add parser and binding definitions.

---
+++ src/binding_def.rs
@@ -0,0 +1,68 @@
+use crate::env::Env;
+use crate::expr::Expr;
+use crate::utils;
+
+#[derive(Debug, PartialEq)]
+pub struct BindingDef {
+    name: String,
+    val: Expr,
+}
+
+impl BindingDef {
+    pub fn new(s: &str) -> Result<(&str, Self), String> {
+        let s = utils::tag("let", s)?;
+        let (s, _) = utils::extract_whitespace_wrapper(s)?;
+
+        let (s, name) = utils::extract_ident(s)?;
+        let (s, _) = utils::extract_whitespace(s);
+
+        let s = utils::tag("=", s)?;
+        let (s, _) = utils::extract_whitespace(s);
+
+        let (s, val) = Expr::new(s)?;
+
+        Ok((
+            s,
+            Self {
+                name: name.to_string(),
+                val,
+            },
+        ))
+    }
+
+    pub(crate) fn eval(&self, env: &mut Env) {
+        env.store_binding(self.name.clone(), self.val.eval());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::expr::{Number, Op};
+
+    #[test]
+    fn parse_binding_def() {
+        assert_eq!(
+            BindingDef::new("let a = 10 / 2"),
+            Ok((
+                "",
+                BindingDef {
+                    name: "a".to_string(),
+                    val: Expr::Operation {
+                        lhs: Number(10),
+                        rhs: Number(2),
+                        op: Op::Div,
+                    },
+                },
+            )),
+        );
+    }
+
+    #[test]
+    fn cannot_parse_binding_def_without_space_after_let() {
+        assert_eq!(
+            BindingDef::new("letaaa=1+2"),
+            Err("expected a space".to_string()),
+        );
+    }
+}
